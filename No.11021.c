b4ff46251be8140f3933d5f8efb12229c4635d65
HYEON2001 <sehyeon73@gmail.com>
2020-10-27 15:00:42 +0900

    A+B - 7

---
+++ No.11021.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int a, b, T, i;
+
+int main()
+{
+	scanf("%d", &T);
+	for(i = 1; i <= T; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n",i, a + b);
+	}
+	return 0;
+}
