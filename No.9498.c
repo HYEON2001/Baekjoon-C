6d7260272beaa2d17fee323cb172eac257ca6c91
HYEON2001 <sehyeon73@gmail.com>
2020-11-08 19:36:06 +0900

    시험 성적

---
+++ No.9498.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int s;
+
+int main()
+{
+	scanf("%d", &s);
+	if(s >= 90)
+		printf("A");
+	else if(s >= 80)
+		printf("B");
+	else if(s >= 70)
+		printf("C");
+	else if(s >= 60)
+		printf("D");
+	else
+		printf("F");
+	return 0;
+}
